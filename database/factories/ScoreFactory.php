2387ec85e64d81bc26a9da63cfea4fce82772438
Wxld-dev <dev.wxld@protonmail.com>
2022-05-24 17:58:20 -0300

    Added factories

--- database/factories/ScoreFactory.php
+++ database/factories/ScoreFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Score>
  */
-class GameFactory extends Factory
+class ScoreFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,10 @@ class GameFactory extends Factory
     public function definition()
     {
         return [
-            'name' => 'Test Game'
+            "user_id" => rand(1,10),
+            "game_id" => rand(1,2),
+            "tournament_id" => rand(1,2),
+            "score" => rand(1,100)
         ];
     }
 }
